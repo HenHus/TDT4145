4924fd6c7eb37f461341f02ae69279eeb66d390c
Henning Husmo Kristiansen <henninhk@stud.ntnu.no>
2025-03-09 11:36:38 +0100

    Refactor Flyplass database schema: update column names for consistency and clarity in Flytype, Fly, Flyrute, Flyvning, FlyPriser, Billett, KjoptBillett, and Billettkjop tables

--- FlyplassSetup.sql
+++ FlyplassSetup.sql
@@ -20,39 +20,39 @@ CREATE TABLE Flyprodusent (
 -- Tabell for Flytyper
 CREATE TABLE Flytype (
     Navn TEXT PRIMARY KEY,
-    Produsent TEXT NOT NULL,
+    Flyprodusent TEXT NOT NULL,
     Produksjonstart INTEGER NOT NULL,
     Produksjonsslutt INTEGER,
     Setekapasitet INTEGER NOT NULL,
-    FOREIGN KEY (Produsent) REFERENCES Flyprodusent(Navn) ON UPDATE CASCADE ON DELETE CASCADE
+    FOREIGN KEY (Flyprodusent) REFERENCES Flyprodusent(Navn) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 -- Tabell for Fly
 CREATE TABLE Fly (
     RegNr TEXT PRIMARY KEY,
-    SerNr TEXT NOT NULL,
+    SerieNr TEXT NOT NULL,
     Navn TEXT,
     DriftetSiden INTEGER NOT NULL,
     Flytype TEXT NOT NULL,
     Flyselskap TEXT NOT NULL,
     FOREIGN KEY (Flytype) REFERENCES Flytype(Navn) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (Flyselskap) REFERENCES Flyselskap(Flyselskapkode) ON UPDATE CASCADE ON DELETE CASCADE,
-    UNIQUE (SerNr, Flyselskap)
+    UNIQUE (SerieNr, Flyselskap)
 );
 
 -- Tabell for Flyruter
 CREATE TABLE Flyrute (
-    RuteNr TEXT PRIMARY KEY,
+    Flyrutenummer TEXT PRIMARY KEY,
     StartFlyplass TEXT NOT NULL,
     SluttFlyplass TEXT NOT NULL,
     Ukedagskode TEXT NOT NULL,
     Flyselskap TEXT NOT NULL,
     Flytype TEXT NOT NULL,
-    Avgangstid TEXT NOT NULL,
-    Ankomsttid TEXT NOT NULL,
+    PlanlagtAvgang TEXT NOT NULL,
+    PlanlagtAnkomst TEXT NOT NULL,
     Oppstartsdato TEXT NOT NULL,
     SluttDato TEXT,
-    UNIQUE (RuteNr, Flyselskap),
+    UNIQUE (Flyrutenummer, Flyselskap),
     FOREIGN KEY (Flytype) REFERENCES Flytype(Navn) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (Flyselskap) REFERENCES Flyselskap(Flyselskapkode) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (StartFlyplass) REFERENCES Flyplass(Flyplasskode) ON UPDATE CASCADE ON DELETE CASCADE,
@@ -62,24 +62,24 @@ CREATE TABLE Flyrute (
 -- Tabell for Flyvninger
 CREATE TABLE Flyvning (
     Lopenummer TEXT NOT NULL,
-    RuteNr TEXT NOT NULL,
-    Fly TEXT,
+    Flyrutenummer TEXT NOT NULL,
+    FlyRegNr TEXT NOT NULL,
     FlyStatus TEXT,
     FaktiskAvgangstid TEXT,
     FaktiskAnkomsttid TEXT,
-    PRIMARY KEY (Lopenummer, RuteNr),
-    FOREIGN KEY (RuteNr) REFERENCES Flyrute(RuteNr) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (Fly) REFERENCES Fly(RegNr) ON UPDATE CASCADE ON DELETE CASCADE
+    PRIMARY KEY (Lopenummer, Flyrutenummer),
+    FOREIGN KEY (Flyrutenummer) REFERENCES Flyrute(Flyrutenummer) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (FlyRegNr) REFERENCES Fly(RegNr) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 -- Tabell for Priser
 CREATE TABLE FlyPriser (
     Lopenummer TEXT NOT NULL,
-    RuteNr TEXT NOT NULL,
+    Flyrutenummer TEXT NOT NULL,
     Kategori TEXT,
     Pris INTEGER NOT NULL,
-    PRIMARY KEY (Lopenummer, RuteNr, Kategori),
-    FOREIGN KEY (Lopenummer, RuteNr) REFERENCES Flyvning(Lopenummer, RuteNr) ON UPDATE CASCADE ON DELETE CASCADE
+    PRIMARY KEY (Lopenummer, Flyrutenummer, Kategori),
+    FOREIGN KEY (Lopenummer, Flyrutenummer) REFERENCES Flyvning(Lopenummer, Flyrutenummer) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 -- Tabell for Kunder
@@ -88,15 +88,15 @@ CREATE TABLE Kunde (
     Navn TEXT NOT NULL,
     Telefonnummer TEXT NOT NULL,
     Epost TEXT NOT NULL,
-    Nasjoalitet TEXT NOT NULL
+    Nasjonalitet TEXT NOT NULL
 );
 
 -- Tabell for Fordelsprogram
 CREATE TABLE Fordelsprogram (
     Referanse TEXT PRIMARY KEY,
-    Kunde TEXT NOT NULL,
+    KundeNr INTEGER NOT NULL,
     Flyselskap TEXT NOT NULL,
-    FOREIGN KEY (Kunde) REFERENCES Kunde(KundeNr) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (KundeNr) REFERENCES Kunde(KundeNr) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (Flyselskap) REFERENCES Flyselskap(Flyselskapkode) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
@@ -104,19 +104,18 @@ CREATE TABLE Fordelsprogram (
 CREATE TABLE Billett (
     BillettId INTEGER PRIMARY KEY,
     Lopenummer TEXT NOT NULL,
-    Flyrute TEXT NOT NULL,
-    Kategori NOT NULL,
-    KjopsPris TEXT NOT NULL,
+    Rutenummer TEXT NOT NULL,
+    Kategori TEXT NOT NULL,
     Sete TEXT,
     TidspunktInnsjekk TEXT NOT NULL,
-    FOREIGN KEY (Lopenummer, Flyrute) REFERENCES Flyvning(Lopenummer, RuteNr) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (Billettkjop) REFERENCES Billettkjop(Referansenummer) ON UPDATE CASCADE ON DELETE CASCADE
+    FOREIGN KEY (Lopenummer, Rutenummer) REFERENCES Flyvning(Lopenummer, Flyrutenummer) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 -- Tabell for kjøpte billetter
 CREATE TABLE KjoptBillett (
     BillettId INTEGER PRIMARY KEY,
     Referansenummer TEXT NOT NULL,
+    Kjopspris INTEGER NOT NULL,
     FOREIGN KEY (BillettId) REFERENCES Billett(BillettId) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (Referansenummer) REFERENCES Billettkjop(Referansenummer) ON UPDATE CASCADE ON DELETE CASCADE
 );
@@ -125,12 +124,14 @@ CREATE TABLE KjoptBillett (
 CREATE TABLE Billettkjop (
     Referansenummer TEXT PRIMARY KEY,
     KundeNr INTEGER NOT NULL,
+    Rutenummer TEXT NOT NULL,
     Startflyplass TEXT NOT NULL,
     Sluttflyplass TEXT NOT NULL,
     Totalpris INTEGER NOT NULL,
     FOREIGN KEY (KundeNr) REFERENCES Kunde(KundeNr) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (Startflyplass) REFERENCES Flyplass(Flyplasskode) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (Sluttflyplass) REFERENCES Flyplass(Flyplasskode) ON UPDATE CASCADE ON DELETE CASCADE
+    FOREIGN KEY (Sluttflyplass) REFERENCES Flyplass(Flyplasskode) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (Rutenummer) REFERENCES Flyrute(Flyrutenummer) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 -- Tabell for Bagasje
