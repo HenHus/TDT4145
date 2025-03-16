74648ea2baf1202fccf2d28e90d9883174e3caf3
Birk Thomassen <birk.ole.thomassrn@gmail.com>
2025-03-16 15:03:57 +0100

    feat: update intro function to include new flight booking option and add find_seats functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sqlite3
 import AvangerOgAnkomster as AOA
 import Oversikt
+import find_seats
 
 # Funksjon som legger inn data basert på veldeggene i prosjektoppgaven i databasen
 def insert_data(sql_file):
@@ -21,10 +22,11 @@ def insert_data(sql_file):
 # Intro til programmet
 def intro():
     print("\n\n\n\nVi har følgende funksjonaliteter:", end="\n\n\n")
-    print("1 -- Legg in data i databasen fra vedlegg 1, 2 og 3", end="\n\n")
+    print("1 -- Legg in data i databasen fra vedlegg 1, 2 og 3. Legg inn flyvninger for 1. april 2025.", end="\n\n")
     print("2 -- En enklel oversikt over flyselskap, flytyper som flyselskaper bruker, og hvor mange fly av flytypene (SQL-spørring)", end="\n\n")
     print("3 -- Miniprogram for oversikt over avganger og ankomster til en gitt flyplass på en gitt dag", end="\n\n")
-    print("4 -- Minoprogram for oversikt over ledige seter på en flyvning", end="\n\n")
+    print("4 -- Legg inn 10 bestillinger for WF1302 (BOO-TRD) tirsdag 1. april 2025", end="\n\n")
+    print("5 -- Minoprogram for oversikt over ledige seter på en flyvning", end="\n\n")
 
 # Hovedprogrammet som kjøres i terminalen
 def main():
@@ -45,8 +47,8 @@ def main():
             AOA.HentAvgangerOgAnkomster()
         elif valg == "4":
             pass
+        elif valg == "5":
+            find_seats.main()
         else:
             print("Ugyldig valg. Prøv igjen!.")
-
-
 main()
