431c641b3a659762dd890b084b64fc6984ec021d
Henning Husmo Kristiansen <henninhk@stud.ntnu.no>
2025-03-16 12:45:41 +0100

    feat: add data insertion functionality from SQL file and update menu options

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
+import sqlite3
 import AvangerOgAnkomster as AOA
 import Oversikt
 
+def insert_data(sql_file):
+
+    conn = sqlite3.connect("Fly.db")
+    try:
+        with open(sql_file, 'r', encoding='utf-8') as file:
+            sql_script = file.read()
+        conn.executescript(sql_script)
+        conn.commit()
+        print("Data fra vedleggene ble lagt inn.")
+    except Exception as e:
+        conn.rollback()
+        print(f"Error: {e}")
+    
+    conn.close()
+
 def intro():
     print("\n\n\n\nVi har følgende funksjonaliteter:", end="\n\n\n")
-    print("1 -- En enklel oversikt over flyselskap, flytyper som flyselskaper bruker, og hvor mange fly av flytypene (SQL-spørring)", end="\n\n")
-    print("2 -- Oversikt over avganger og ankomster til en gitt flyplass på en gitt dag", end="\n\n")
+    print("1 -- Legg in data i databasen fra vedlegg 1, 2 og 3", end="\n\n")
+    print("2 -- En enklel oversikt over flyselskap, flytyper som flyselskaper bruker, og hvor mange fly av flytypene (SQL-spørring)", end="\n\n")
+    print("3 -- Oversikt over avganger og ankomster til en gitt flyplass på en gitt dag", end="\n\n")
 
 
 def main():
@@ -18,8 +35,10 @@ def main():
             print("Goodbye.")
             break
         elif valg == "1":
-            Oversikt.main()
+            insert_data("FlyplassData.sql")
         elif valg == "2":
+            Oversikt.main()
+        elif valg == "3":
             AOA.HentAvgangerOgAnkomster()
 
 
